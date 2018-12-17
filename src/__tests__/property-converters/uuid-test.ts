629bcbe90eb79b49a435278559382f6a80dc2e42
Erik Christensen <erikchristensen@comcast.net>
2018-12-16 23:54:16 -0500

    Added UUID test - needs work still

---
+++ src/__tests__/property-converters/uuid-test.ts
@@ -0,0 +1,43 @@
+import { UuidConverter } from '../../property-converters'
+import { Binary } from 'bson';
+
+describe('UUID Converter', () => {
+    let converter: UuidConverter
+
+    beforeEach(() => {
+        converter = new UuidConverter()
+    })
+
+    it('should convert undefined values to DB', () => {
+        expect(converter.toDb(undefined)).toBeUndefined()
+    })
+
+    it('should convert null values to DB', () => {
+        expect(converter.toDb(null)).toBeNull()
+    })
+
+    it('should convert valid string UUIDs to DB', () => {
+        const toDbValue = converter.toDb("0d46691c-c7c5-4f38-9aad-fe2ecfca8ef2")
+        expect(toDbValue).toBeInstanceOf(Binary)
+        expect((toDbValue as Binary).sub_type).toBe(Binary.SUBTYPE_UUID)
+        // expect((toDbValue as Binary).buffer).toEqual(Binary.SUBTYPE_UUID)
+    })
+
+    it('should convert undefined values from DB', () => {
+        expect(converter.fromDb(undefined)).toBeUndefined()
+    })
+
+    it('should convert null values from DB', () => {
+        expect(converter.fromDb(null)).toBeNull()
+    })
+
+    // it('should convert Double values from DB', () => {
+    //     const fromDbValue = converter.fromDb(new Double(50.0))
+    //     expect(typeof fromDbValue).toEqual('number')
+    //     expect(fromDbValue).toEqual(50.0)
+    // })
+
+    // it('should fail to convert numbers from DB', () => {
+    //     expect(() => converter.fromDb(50.0)).toThrow(Error)
+    // })
+})
